9336b1d5b4411fd0f6f14bc56336819180466590
Kunitsa3 <Kunitsa3@tut.by>
2021-07-28 22:40:26 +0300

    Change router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Router, Switch } from 'react-router-dom';
+import { BrowserRouter, Router, Switch } from 'react-router-dom';
 import { renderRoutes } from './routing';
 import { createBrowserHistory } from 'history';
 
@@ -7,9 +7,9 @@ export const history = createBrowserHistory();
 function App() {
   return (
     <div className="App">
-      <Router history={history}>
+      <BrowserRouter>
         <Switch>{renderRoutes()}</Switch>
-      </Router>
+      </BrowserRouter>
     </div>
   );
 }
